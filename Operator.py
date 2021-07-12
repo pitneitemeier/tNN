465ae4e2f4da1c46701b6c6357e2facec961fa29
pitneitemeier <pit.neitemeier@rwth-aachen.de>
2021-07-12 20:07:07 +0000

    added utils to calc Oloc and Operator class

---
+++ Operator.py
@@ -0,0 +1,83 @@
+import collections
+import torch
+
+class Operator:
+  def __init__(self, lat_site):
+    self.lat_site = lat_site
+    self.switching_map = 1
+    self.mat_els = torch.tensor([1., 1.])
+
+    self.name = 'not yet named'
+
+  def __str__(self):
+    return self.name+f"_{self.lat_site}"
+
+  #multiplication of operator: tuple
+  #sum of operators: list
+  def __mul__(self, other):
+    if isinstance(other, (float, int)):
+      self.mat_els *= other
+      return self
+    elif isinstance(other, Operator):
+      if other.lat_site == self.lat_site:
+        print('multiplication of operators on same lattice site nyi')
+        return self
+      else:
+        return (self,other)
+    else:
+      print('multiplication for this type nyi')
+      return self
+
+
+  def __add__(self, other):
+    if isinstance(other, Operator):
+      return [(self), (other)]
+    elif isinstance(other, collections.Sequence):
+      if (type(other) == list):
+        if (len(other) == 0 or isinstance(other[0][0], Operator)):
+          print('adding to sequence')
+          return  [(self,)] + other
+        else:
+          print('cant add to sequence, wrong type')
+      elif (type(other) == tuple):
+        if (isinstance(other[0], Operator)):
+          return [(self,), other]
+        else:
+          print('cant add to sequence, wrong type')
+    else:
+      print('add for this type nyi')
+      return self
+  
+
+
+class Sx(Operator):
+  def __init__(self, lat_site):
+    super().__init__(lat_site)
+    self.switching_map = -1
+    self.mat_els = torch.tensor([1., 1.], dtype=torch.complex64)
+    self.name = 'Sx'
+
+class Sy(Operator):
+  def __init__(self, lat_site):
+    super().__init__(lat_site)
+    self.switching_map = -1
+    self.mat_els = torch.tensor([1.j, -1.j], dtype=torch.complex64)
+    self.name = 'Sy'
+
+class Sz(Operator):
+  def __init__(self, lat_site):
+    super().__init__(lat_site)
+    self.switching_map = 1
+    self.mat_els = torch.tensor([1., -1.], dtype=torch.complex64)
+    self.name = 'Sz'
+
+def print_op_list(op_list):
+  out_str = "Hamiltonan = "
+  for j, op_tuple in enumerate(op_list):
+    if (j>0 and j<len(op_list)):
+      out_str += (" + ")
+    for i, op in enumerate(op_tuple):
+      out_str += str(op)
+      if (i<(len(op_tuple)-1)):
+        out_str += (" * ")
+  print(out_str)
---
+++ utils.py
@@ -0,0 +1,80 @@
+import torch
+import numpy as np
+
+def get_map(hamiltonian, lattice_sites):
+#1.dim: Batch
+#2.dim: Number of summands in Hamilton operator
+#3.dim: number of lattice sites
+  map = torch.ones((1, len(hamiltonian), lattice_sites), dtype=torch.int8)
+  for i, op_tuple in enumerate(hamiltonian):
+    for op in op_tuple:
+      map[:, i, op.lat_site] = op.switching_map
+  return map
+
+
+def get_total_mat_els(hamiltonian, lattice_sites):
+#1.dim: Batch
+#2.dim: two possibilities for mat el depending on input first=+1, second=-1
+#3.dim: Number of summands in Hamilton operator
+#4.dim: number of lattice sites
+  mat_els = torch.full((1, 2, len(hamiltonian), lattice_sites), 1, dtype=torch.complex64)
+  for i, op_tuple in enumerate(hamiltonian):
+    for op in op_tuple:
+      mat_els[:, :, i, op.lat_site] = op.mat_els
+  return mat_els
+
+
+def get_one_hot(spin_config):
+#1.dim: Batch
+#2.dim: two possibilities for mat el depending on input: first=+1, second=-1
+#3.dim: Number of summands in Hamilton operator
+#4.dim: number of lattice sites
+  return torch.stack((0.5*(spin_config+1), 0.5*(1-spin_config))).reshape(spin_config.shape[0], 2, 1, spin_config.shape[1])
+
+
+#getting s_primes for O_loc via map
+def get_sp(spin_config, map):
+  #returns:
+  #1. dim: batch size
+  #2. dim: number of summands in h = number of sp configs per s_config
+  #3. dim: lattice_sites
+  return map*spin_config.reshape(spin_config.shape[0], 1, spin_config.shape[1])
+
+
+def calc_Oloc(psi_sp, psi_s, mat_els, spin_config):
+  #using onehot encoding of spin config to select the correct mat_el from all mat_els table by multiplication
+  s_onehot = get_one_hot(spin_config)
+  #summing out zeroed values to only
+  res = (mat_els * s_onehot).sum(1)
+  #product of all matrix elements for one summand of the hamiltonian.
+  res = res.prod(2)
+  #multiplying with corresponding weights and summing over s' for each input configuration
+  print(res)
+  res = (res / psi_s * psi_sp).sum(1)
+  return res
+
+
+from itertools import permutations
+#get all basis configurations to calculate stochastic mean
+def get_all_spin_configs(num_lattice_sites):
+  perm = np.zeros( (np.power( 2, num_lattice_sites ), num_lattice_sites) )
+  currently_filled = 0
+  for i in range(num_lattice_sites + 1):
+    tmp = np.ones(num_lattice_sites)
+    tmp[i:] = -1
+    tmp = np.array(list(set(permutations(tmp))))
+    for row in tmp:
+      perm[currently_filled, :] = row
+      currently_filled += 1
+  return torch.from_numpy(perm)
+
+
+def get_corr_stoch_mean(psi_s, obs_a_k_s, obs_b_k_s):
+  # (Oa_k * Ob_k')s - (Oa_k)s * (Ob_k')s
+  # make matrix of Oak,Obk' and (Oak)s(Obk')s
+  # returns:
+  # stoch mean 
+  # 1. dim: k
+  # 2. dim: k'
+  pass
+  
