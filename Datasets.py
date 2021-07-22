fixed bug in scaling of mat els for ext param

Datasets.py
@@ -6,9 +6,9 @@ import utils
 import Operator as op
 
 class Train_Data(Dataset):
-    def __init__(self, lattice_sites, h_mat_list, h_ranges_list, o_mat, g_dtype, t_min=0, t_max=1):
+    def __init__(self, lattice_sites, num_op_h_list, h_ranges_list, g_dtype, t_min=0, t_max=1):
         
-        assert(len(h_mat_list) - 1 == len(h_ranges_list))
+        assert(len(num_op_h_list) -1 == len(h_ranges_list))
         #exact sampling for now
         self.spins = utils.get_all_spin_configs(lattice_sites).type(g_dtype)
         self.g_dtype = g_dtype
@@ -18,8 +18,8 @@ class Train_Data(Dataset):
         self.h_ranges_list = h_ranges_list
 
         #saving mat elements to pass to training loop with respective multipliers each loop
-        self.h_mat_list = h_mat_list
-        self.o_mat = o_mat
+        self.num_op_h_list = num_op_h_list
+        self.num_summands_h = int(torch.tensor(num_op_h_list).sum())
     
         
     def __len__(self):
@@ -27,35 +27,52 @@ class Train_Data(Dataset):
         return 100000
 
     def __getitem__(self, index):
+        '''
+        Parameters
+        ----------
+        index : int
+            dummy batch index
+        Returns
+        -------
+        spins : tensor
+            shape = (num_spin_configs, lattice_sites)
+        alpha_arr : tensor
+            shape = (num_spin_configs, num_ext_params + time)
+        alpha_0 : tensor
+            same as alpha_arr but with t=0 to satisfy init cond
+        ext_param_scale : tensor
+            tensor to scale matrix elements of hamiltonian according to external parameters
+            shape = (num_spin_configs, num_summands_h, 1)
+        '''
         #creating the random alpha array of numspins with one value of (t, h_ext1, ..)
-        alpha_arr = torch.full((self.spins.shape[0], (len(self.h_mat_list))), 0, dtype=self.g_dtype) 
+        alpha_arr = torch.full((self.spins.shape[0], (len(self.num_op_h_list))), 0, dtype=self.g_dtype) 
+        ext_param_scale = torch.ones((self.spins.shape[0], self.num_summands_h), dtype=self.g_dtype)
         if self.h_ranges_list is not None:
-            for i in range( len(self.h_mat_list) - 1 ):
+            current_ind = self.num_op_h_list[0]
+            for i in range( len(self.num_op_h_list) - 1 ):
                 max = self.h_ranges_list[i][1]
                 min = self.h_ranges_list[i][0] 
-                alpha_arr[:, i+1] = ( (max - min) * torch.rand((1,1)) + min )
+                randval = ( (max - min) * torch.rand((1,1)) + min )
+                ext_param_scale[:, current_ind : current_ind + self.num_op_h_list[i+1]] = randval.unsqueeze(0)
+                alpha_arr[:, i+1] = randval
+                current_ind += self.num_op_h_list[i+1]
         alpha_0 = alpha_arr.clone()
         alpha_arr[:, 0] = ( ( self.t_max - self.t_min ) * torch.rand((1,1)) + self.t_min )
-        h_mat = self.h_mat_list[0]
-        for i in range(len(self.h_mat_list) - 1):
-            h_mat = torch.cat((h_mat, alpha_arr[0, i +1] * self.h_mat_list[i + 1]), dim=2)
 
-        return self.spins, alpha_arr, alpha_0, h_mat, self.o_mat
+        return self.spins, alpha_arr, alpha_0, ext_param_scale
 
 
 class Val_Data(Dataset):
-    def __init__(self, lattice_sites, ED_data, o_mat, g_dtype, ext_params = None):
+    def __init__(self, lattice_sites, ED_data, ext_params, g_dtype):
         #exact sampling for now
         self.spins = utils.get_all_spin_configs(lattice_sites).type(g_dtype)
-        #target Magnetizations from ED Code that 
+        #target Magnetizations and corresponding times from ED Code
         self.t_arr = torch.from_numpy(ED_data[0, :, 0]).type(g_dtype).unsqueeze(1)
         #print('t_arr shape', self.t_arr.shape)
         self.O_target = torch.from_numpy(ED_data[:, :, 1]).type(g_dtype)
 
         #saving mat elements to pass to val loop
-        self.o_mat = o_mat
         self.ext_params = torch.from_numpy(ext_params).type(g_dtype)
-        
         
     def __len__(self):
         #just full batch training here with all t
@@ -63,21 +80,19 @@ class Val_Data(Dataset):
 
     def __getitem__(self, index):
         t_arr = self.t_arr.repeat(1, self.spins.shape[0]).unsqueeze(2)
-        if self.ext_params is not None:
-            ext_params = self.ext_params[index,:].broadcast_to(t_arr.shape[0], self.spins.shape[0], 1)
-            #print('repeated t_arr shape ',t_arr.shape)
-            #print('repeated_ext shape ', ext_params.shape)
-            alpha = torch.cat((t_arr, ext_params), dim=2)
-        else:
-            alpha = t_arr
+        ext_params = self.ext_params[index,:].broadcast_to(t_arr.shape[0], self.spins.shape[0], 1)
+        #print('repeated t_arr shape ',t_arr.shape)
+        #print('repeated_ext shape ', ext_params.shape)
+        alpha = torch.cat((t_arr, ext_params), dim=2)
         spins = self.spins.unsqueeze(0).repeat(self.t_arr.shape[0], 1, 1)
-        return spins, alpha, self.o_mat, self.O_target[index]
+        return spins, alpha, self.O_target[index]
 
 
 
 if (__name__ == '__main__'):
+    
     ###setting up hamiltonian ###
-    lattice_sites = 3
+    lattice_sites = 4
     g_dtype = torch.float64
 
     h2_range = [(0.15, 1.35)]
@@ -101,6 +116,7 @@ if (__name__ == '__main__'):
     h_map = utils.get_map(h1 + h2, lattice_sites)
     o_map = utils.get_map(o, lattice_sites)
 
+    '''
     ED_data_02 = np.loadtxt('ED_data/ED_data4_02.csv', delimiter=',')
     ED_data_05 = np.loadtxt('ED_data/ED_data4_05.csv', delimiter=',')
     ED_data_07 = np.loadtxt('ED_data/ED_data4_07.csv', delimiter=',')
@@ -118,3 +134,11 @@ if (__name__ == '__main__'):
     print('o_mat_shape', o_mat.shape)
     print('o_target shape', o_target.shape)
     print('alpha shape', alpha.shape)
+    '''
+    print(len(h1), len(h2))
+    train_data = Train_Data(4, [len(h1), len(h2)], [(0.9, 1.1)], torch.float64)
+    train_dataloader = DataLoader(train_data, 2)
+    train_iter = iter(train_dataloader)
+    spins, alpha_arr, alpha_0, ext_param_scale = next(train_iter)
+    print('ext_param_scale', ext_param_scale[:, 0, :])
+    print('alpha: ', alpha_arr[:, 0, 1])

model.py
@@ -20,7 +20,7 @@ g_dtype = torch.float64
 ###setting up hamiltonian ###
 lattice_sites = 4
 
-h2_range = [(0.95, 1.05)]
+h2_range = [(0.45, 0.55)]
 
 h1 = []
 for l in range(lattice_sites):
@@ -36,6 +36,7 @@ for l in range(lattice_sites):
 
 h1_mat = utils.get_total_mat_els(h1, lattice_sites)
 h2_mat = utils.get_total_mat_els(h2, lattice_sites)
+h_mat = torch.cat((h1_mat, h2_mat), dim=-2)
 o_mat = utils.get_total_mat_els(o, lattice_sites)
 
 h_map = utils.get_map(h1 + h2, lattice_sites)
@@ -73,33 +74,31 @@ ED_data_13 = np.loadtxt(path + '13' + fmt, delimiter=',')
 #ED_data = np.stack((ED_data_02, ED_data_05, ED_data_07, ED_data_10, ED_data_13))
 #ext_params = np.array([0.9, 1., 1.1]).reshape(3,1)
 #ED_data = np.stack((ED_data_09, ED_data_10, ED_data_11))
-ED_data = np.expand_dims(ED_data_10, 0)
-ext_params = np.array([1.]).reshape(1,1)
-val_data = Datasets.Val_Data(lattice_sites, ED_data, o_mat, g_dtype, ext_params)
+ED_data = np.expand_dims(ED_data_05, 0)
+ext_params = np.array([.5]).reshape(1,1)
+
+val_data = Datasets.Val_Data(lattice_sites, ED_data, ext_params, g_dtype)
 val_dataloader = DataLoader(val_data, batch_size=1, num_workers=24)
 val_iter = iter(val_dataloader)
 
-train_data = Datasets.Train_Data(lattice_sites, [h1_mat, h2_mat], h2_range, o_mat, g_dtype)
+train_data = Datasets.Train_Data(lattice_sites, [len(h1), len(h2)], h2_range, g_dtype)
 train_dataloader = DataLoader(dataset=train_data, batch_size=200, num_workers=24)
 data_iter = iter(train_dataloader)
 
 
 class Model(pl.LightningModule):
 
-  def __init__(self, lattice_sites, h_map, o_init_map, t_min, t_max):
+  def __init__(self, lattice_sites, h_mat, h_map, o_mat, o_init_map, t_min, t_max):
     '''
     Initializer for neural net
     Parameters
     __________
     lattice_sites: int
-    h_mat_list: tensor, dtype=complex
-      the matrix elements of the hamiltonian 
-      shape = (num_ext_params, )
     '''
     super().__init__()
 
     lattice_hidden = 64
-    mult_size = 64
+    mult_size = 128
     self.lattice_net = nn.Sequential(
       nn.Linear( lattice_sites, lattice_hidden, dtype=g_dtype),
       nn.CELU(),
@@ -133,6 +132,8 @@ class Model(pl.LightningModule):
     self.flatten_spins = nn.Flatten(end_dim=-1)
     self.h_map = h_map.to(device)
     self.o_init_map = o_init_map.to(device)
+    self.h_mat = h_mat.to(device)
+    self.o_mat = o_mat.to(device)
     self.t_min = t_min
     self.t_max = t_max
 
@@ -202,10 +203,10 @@ class Model(pl.LightningModule):
     return psi.reshape( sprimes_shape[0], sprimes_shape[1], sprimes_shape[2], 1)
   
   def training_step(self, batch, batch_idx):
-    spins, alpha, alpha_0, h_mat, o_mat = batch
+    spins, alpha, alpha_0, ext_param_scale = batch
 
     #calculate dynamic end time and decay rate for loss
-    n = int(self.current_epoch / 2) + 1
+    n = int(self.current_epoch / 3) + 1
     N = 10
     t_max = self.t_min + (self.t_max - self.t_min) * np.log(10 * n / N + 1) / np.log( 11 )
     #t_max = self.t_max
@@ -225,7 +226,7 @@ class Model(pl.LightningModule):
     sp_h = utils.get_sp(spins, self.h_map)
     psi_sp_h = self.call_forward_sp(sp_h, alpha)
     #calc h_loc for h
-    h_loc = utils.calc_Oloc(psi_sp_h, h_mat, spins)
+    h_loc = utils.calc_Oloc(psi_sp_h, self.h_mat, spins, ext_param_scale)
     dt_psi_s = utils.calc_dt_psi(psi_s, alpha)
     
     #get s' and psi(s', alpha) for o at t=0
@@ -234,7 +235,7 @@ class Model(pl.LightningModule):
     psi_s_0 = self.call_forward(spins, alpha_0)
     
     #calc o_loc for o
-    o_loc = utils.calc_Oloc(psi_sp_o, o_mat, spins)
+    o_loc = utils.calc_Oloc(psi_sp_o, self.o_mat, spins)
 
     #calc loss
     loss, schrodinger, init_cond, norm = utils.train_loss2(dt_psi_s, h_loc, psi_s, psi_s_0, o_loc, alpha, loss_weight)
@@ -244,24 +245,23 @@ class Model(pl.LightningModule):
     self.log('train_loss', loss, logger=True)
 
     return {'loss': loss}
-
+  
   def validation_step(self, batch, batch_idx):
-    spins, alpha, o_mat, o_target = batch
-    n = int(self.current_epoch / 2) + 1
+    spins, alpha, o_target = batch
+    n = int(self.current_epoch / 3) + 1
     N = 10
     t_max = self.t_min + (self.t_max - self.t_min) * np.log(10 * n / N + 1) / np.log( 11 ) + 0.2
     #t_max = self.t_max    
     max_ind = torch.argmin(torch.abs(alpha[0, :, 0, 0] - t_max))
     spins = spins[0, :max_ind, :, :]
     alpha = alpha[0, :max_ind, :, :]
-    o_mat = o_mat[0, :]
     o_target = o_target[0, :max_ind]
     
 
     psi_s = self.call_forward(spins, alpha)
     sp_o = utils.get_sp(spins, self.o_init_map)
     psi_sp_o = self.call_forward_sp(sp_o, alpha)
-    o_loc = utils.calc_Oloc(psi_sp_o, o_mat, spins)
+    o_loc = utils.calc_Oloc(psi_sp_o, self.o_mat, spins)
     val_loss, observable = utils.val_loss(psi_s, o_loc, o_target)
     #self.log('val_loss', val_loss, prog_bar=True, logger=True)
 
@@ -269,7 +269,7 @@ class Model(pl.LightningModule):
     #print('alpha_shape', alpha.shape)
     
     return {'observable' : observable, 'time': alpha[:, 0, 0], 'target': o_target, 'ext_param': alpha[0,0,1]}
-  
+
   def validation_epoch_end(self, outs):
     fig, ax = plt.subplots(figsize=(10,6))
     for i, out in enumerate(outs):
@@ -283,7 +283,7 @@ class Model(pl.LightningModule):
     self.logger.experiment.add_figure('magnetization', fig)
     fig.savefig('magnetization.png')
     plt.close(fig)
-
+  
   def configure_optimizers(self):
     #optimizer = torch.optim.LBFGS(self.parameters(), lr=1e-2)
     optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
@@ -292,9 +292,11 @@ class Model(pl.LightningModule):
     return optimizer
 
 
-model = Model(lattice_sites, h_map, o_map, t_min=0, t_max=1.5)
-print(model)
+model = Model(lattice_sites, h_mat, h_map, o_mat, o_map, t_min=0, t_max=2)
+#print(model)
 
-trainer = pl.Trainer(fast_dev_run=False, gpus=1, max_epochs=20)
+trainer = pl.Trainer(fast_dev_run=False, gpus=1, max_epochs=30)
 trainer.fit(model, train_dataloader, val_dataloader)
+
+
 

utils.py
@@ -26,10 +26,10 @@ def get_total_mat_els(operator, lattice_sites):
 #2.dim: two possibilities for mat el depending on input first=+1, second=-1
 #3.dim: Number of summands in operator
 #4.dim: number of lattice sites
-  mat_els = torch.ones((1, 2, len(operator), lattice_sites), dtype=torch.complex64)
+  mat_els = torch.ones((1, 1, 2, len(operator), lattice_sites), dtype=torch.complex64)
   for i, op_tuple in enumerate(operator):
     for op in op_tuple:
-      mat_els[:, :, i, op.lat_site] = op.mat_els
+      mat_els[:, :, :, i, op.lat_site] = op.mat_els
   return mat_els
 
 
@@ -85,7 +85,7 @@ def get_sp(spin_config, map):
   return map*spin_config
 
 
-def calc_Oloc(psi_sp, mat_els, spin_config):
+def calc_Oloc(psi_sp, mat_els, spin_config, ext_param_scale = None):
   '''
   Calculates the local Operator value
   Parameters
@@ -97,6 +97,9 @@ def calc_Oloc(psi_sp, mat_els, spin_config):
     shape = (num_alphas, num_spin_configs, 2, num_sp, num_lattice_sites)
   spin_config : tensor
     shape = (num_alphas, num_spin_configs, lattice_sites)
+  ext_param_scale : tensor
+    tensor with the external parameters of the hamiltonian to scale up matrix elements
+    shape = (num_alphas, num_spin_configs, num_sp, 1)
   Returns
   -------
   O_loc : tensor
@@ -107,11 +110,21 @@ def calc_Oloc(psi_sp, mat_els, spin_config):
   #using onehot encoding of spin config to select the correct mat_el from all mat_els table by multiplication
   s_onehot = get_one_hot(spin_config)
   #summing out zeroed values in dim 2
+  #print('mat_els shape ', mat_els.shape)
+  #print('s_onehot shape ', s_onehot.shape)
   res = (mat_els * s_onehot).sum(2)
-  
+  #print('res_shape', res.shape)
   #product of all matrix elements for one summand of the hamiltonian.
   res = res.prod(3)
-  #multiplying with corresponding weights and summing over s' for each input configuration
+  #print('res_shape', res.shape)
+
+  #scaling all summands of the hamiltonian with external params
+  if ext_param_scale is not None:
+    #print('res_shape', res.shape)
+    #print('ext_params_scale shape', ext_param_scale.shape)
+    res *= ext_param_scale
+
+  #multiplying with corresponding wave function and summing over s' for each input configuration
   #print("res, psi_sp shape: ",res.shape, psi_sp.shape)
   O_loc = (torch.conj(res.unsqueeze(3)) * psi_sp).sum(2)
   return O_loc
@@ -158,28 +171,7 @@ def psi_norm(psi_s):
   '''
   return (torch.abs(psi_s)**2).sum(1)
 
-def train_loss(dt_psi_s, h_loc, psi_s, psi_s_0, o_loc, alpha):
-  #TODO Documentation
-  h_loc_sq_sum = (torch.abs(h_loc)**2).sum(1)
-  dt_psi_sq_sum = (torch.abs(dt_psi_s)**2).sum(1)
-  dt_psi_h_loc_sum = (torch.abs((torch.conj(dt_psi_s) * h_loc).sum(1))**2)
 
-  abs_val = torch.mean( torch.exp(- alpha[:, 0, 0]) * (torch.abs( dt_psi_sq_sum - h_loc_sq_sum ))**2 )
-  angle = torch.mean( -1 * torch.exp(- alpha[:, 0, 0]) * dt_psi_h_loc_sum / (dt_psi_sq_sum * h_loc_sq_sum) )
-  #exact_angle = torch.mean( torch.acos(torch.sqrt(torch.real(dt_psi_h_loc_sum / (dt_psi_sq_sum * h_loc_sq_sum)))))
-  #print('exact_angle: ', exact_angle)
-  psi_s_0_sq_sum = (torch.abs(psi_s_0)**2).sum(1)
-  psi_0_o_loc_sum = (torch.conj(psi_s_0) * o_loc).sum(1)
-
-  init_cond = torch.mean( (torch.abs( (psi_0_o_loc_sum / psi_s_0_sq_sum) - 1)) ** 2 )
-
-  #part to encourage a normed wave fun
-  batched_norm = psi_norm(psi_s)
-  norm = torch.mean( (batched_norm - 1) ** 2 )
-
-  #return (-angle + abs_val + 1000 * init_cond)
-  return abs_val + angle + 10 * init_cond + norm, angle, init_cond, norm
-  #return init_cond
 
 def train_loss2(dt_psi_s, h_loc, psi_s, psi_s_0, o_loc, alpha, loss_weight):
   #part to satisfy initial condition