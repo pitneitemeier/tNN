4138d054e378015b7af31e4f7f417ba4be746df9
pitneitemeier <pit.neitemeier@rwth-aachen.de>
2021-08-27 17:59:30 +0200

    parametrized step and gamma in FF

--- models.py
+++ models.py
@@ -39,19 +39,20 @@ class parametrized(tNN.Wave_Fun):
         return {'optimizer': optimizer, 'lr_scheduler': lr_scheduler, 'monitor': 'train_loss'}
 
 class ParametrizedFeedForward(tNN.Wave_Fun):
-    def __init__(self, lattice_sites, num_h_params, learning_rate, psi_init, act_fun, kernel_size, num_conv_layers, num_conv_features, tNN_hidden, tNN_num_hidden, mult_size, psi_hidden, psi_num_hidden, init_decay=1, patience=0, optimizer=torch.optim.Adam):
+    def __init__(self, lattice_sites, num_h_params, learning_rate, psi_init, act_fun, kernel_size, 
+            num_conv_layers, num_conv_features, tNN_hidden, tNN_num_hidden, mult_size, psi_hidden, psi_num_hidden, 
+            init_decay=1, step_size=1, gamma=0.1, optimizer=torch.optim.Adam):
         super().__init__(lattice_sites = lattice_sites, name='FF')
         if mult_size % 2 != 0:
             mult_size+=1
         self.save_hyperparameters()
+        self.step_size = step_size
+        self.gamma = gamma
         self.lattice_sites = lattice_sites
         self.psi_init = psi_init
         self.init_decay = init_decay
         self.lr = learning_rate
-        self.patience = patience
         self.opt = optimizer
-        self.scheduler = torch.optim.lr_scheduler.StepLR
-        #self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau
 
         self.lattice_net = act.lattice_block(kernel_size, mult_size, num_conv_features, num_conv_layers, act_fun, self.lattice_sites)
         self.tNN = act.FeedForward(input_size=1+num_h_params, output_size=mult_size, hidden_size=tNN_hidden, num_hidden_layers=tNN_num_hidden, act_fun=act_fun)
@@ -67,8 +68,7 @@ class ParametrizedFeedForward(tNN.Wave_Fun):
 
     def configure_optimizers(self):
         optimizer = self.opt(self.parameters(), lr=self.lr)
-        #lr_scheduler = self.scheduler(optimizer, patience=self.patience, verbose=True, factor=.5)
-        lr_scheduler = self.scheduler(optimizer, step_size=1, verbose=True, gamma=0.1)
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=self.step_size, verbose=True, gamma=self.gamma)
         return {'optimizer': optimizer, 'lr_scheduler': lr_scheduler, 'monitor': 'train_loss'}
 
 class ParametrizedSelfAttention(tNN.Wave_Fun):
@@ -100,6 +100,5 @@ class ParametrizedSelfAttention(tNN.Wave_Fun):
 
     def configure_optimizers(self):
         optimizer = self.opt(self.parameters(), lr=self.lr)
-        #lr_scheduler = self.scheduler(optimizer, patience=self.patience, verbose=True, factor=.5)
         lr_scheduler = self.scheduler(optimizer, step_size=2, verbose=True, gamma=0.5)
         return {'optimizer': optimizer, 'lr_scheduler': lr_scheduler, 'monitor': 'train_loss'}
